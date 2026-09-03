3709941a8ccfda16ea4c53b55a20e91c650d4a51
prasannakondapalli27 <lakshmiprasannakondapalli26@gmal.com>
2026-09-03 15:22:54 +0530

    Day4 Practice

---
+++ day04.py
@@ -0,0 +1,25 @@
+#list
+fruits = ["apple","banana","mango"]
+print(fruits)
+print(fruits[0])
+print(fruits[1])
+
+# adding an item
+fruits = ["apple","banana"]
+fruits.append("mango")
+print(fruits)
+
+
+# tuple
+numbers = (10,20,30,40)
+print(numbers)
+print(numbers[0])
+print(numbers[2])
+
+# tuple with duplicate values
+colors = ("red","blue","red","green")
+print(colors)
+
+# set
+numbers = {10,20,30,20,50}
+print(numbers)
