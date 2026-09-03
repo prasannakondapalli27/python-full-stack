9c3959b808c3ffa5e702d8c25d659a2d679f19dc
prasannakondapalli27 <lakshmiprasannakondapalli26@gmal.com>
2026-09-03 15:59:40 +0530

    Day6 Practice

---
+++ day06.py
@@ -0,0 +1,27 @@
+#string-float
+# price = "99.50"
+
+# price = float(price)
+
+# print(price)
+# print(type(price))
+#inter-string
+# num = 100
+
+# num = str(num)
+
+# print(num)
+# print(type(num))
+#integer-float
+# num = 10
+
+# result = float(num)
+
+# print(result)
+# print(type(result))
+# string to boolean
+name = "Prasanna"
+
+result = bool(name)
+
+print(result)
