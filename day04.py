3e7d855c17f1aa50fe8b6df5484ee6f99143b1a8
prasannakondapalli27 <lakshmiprasannakondapalli26@gmal.com>
2026-09-03 15:49:39 +0530

    Day5 Practice

--- day04.py
+++ day04.py
@@ -1,25 +1,9 @@
-#list
-fruits = ["apple","banana","mango"]
-print(fruits)
-print(fruits[0])
-print(fruits[1])
+student = {
+    "name": "Prasanna",
+    "age": 22,
+    "branch": "ECE"
+}
 
-# adding an item
-fruits = ["apple","banana"]
-fruits.append("mango")
-print(fruits)
-
-
-# tuple
-numbers = (10,20,30,40)
-print(numbers)
-print(numbers[0])
-print(numbers[2])
-
-# tuple with duplicate values
-colors = ("red","blue","red","green")
-print(colors)
-
-# set
-numbers = {10,20,30,20,50}
-print(numbers)
+print(student)
+print(student["name"])
+print(student["age"])
---
+++ day05.py
@@ -0,0 +1,29 @@
+# Creating a dictionary
+# student = {
+#     "name": "Prasanna",
+#     "age": 22,
+#     "branch": "ECE"
+# }
+
+# print(student)
+# print(student["name"])
+# print(student["age"])
+#adding a new value
+# student = {
+#     "name": "Prasanna",
+#     "age": 22
+# }
+
+# student["city"] = "Hyderabad"
+
+# print(student)
+#simple boolean
+# is_student = True
+# is_employee = False
+
+# print(is_student)
+# print(is_employee)
+# even or odd
+num = int(input("Enter a number: "))
+
+print(num % 2 == 0)
