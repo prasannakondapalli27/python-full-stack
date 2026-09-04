fbcd4177f888702d0e22902106654f1c6c5bea36
prasannakondapalli27 <lakshmiprasannakondapalli26@gmal.com>
2026-09-04 15:02:57 +0530

    Day7& C:\Users\konda\AppData\Local\Programs\Python\Python313\python.exe c:/python-full-stack/day07.py Practice

---
+++ day07.py
@@ -0,0 +1,24 @@
+#length of text
+text = "Python"
+print(len(text))
+#indexing
+word = "Hello"
+print(word[0])  
+# accesing the last character
+word = "Hello"
+print(word[-1])  
+#slicing
+word = "Python"
+print(word[1:4])
+#reverse a string
+word = "Python"
+print(word[::-1])  
+#convert text to uppercase
+text = "hello"
+print(text.upper()) 
+#convert text to lowercase
+text = "HELLO"
+print(text.lower())
+# replacing a word
+text = "I like Java"
+print(text.replace("Java", "Python"))
